d3617fe041dba383c4450c92a8173f20553a6e41
Emre <emre.ulkem@outlook.com>
2023-05-26 20:17:13 +0200

    starting

---
+++ LittleLemon/LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by Emre Ülkem on 26.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
